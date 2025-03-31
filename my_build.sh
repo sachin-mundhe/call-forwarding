405451eac6ad85fed9ec348b91aaa6c1e284a956
sachin-mundhe <sachinmundhe77@gmail.com>
2025-03-31 23:46:31 +0530

    fix: dummy build script

---
+++ my_build.sh
@@ -0,0 +1,14 @@
+set -e #exit if any command return non zero status
+set -x #debug mode
+
+# Build docker image
+echo "================== Building an docker image =================="
+
+# login to docker hub
+echo "================== logging into docker hub =================="
+
+
+# push image to docker hub
+echo "================== pushung image to docker hub =================="
+
+
