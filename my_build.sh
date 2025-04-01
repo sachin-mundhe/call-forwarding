037172bfaa95194d95446e4b3f837ff18be7e69f
sachin-mundhe <sachinmundhe77@gmail.com>
2025-04-01 10:29:29 +0530

    fix: added build script

--- my_build.sh
+++ my_build.sh
@@ -1,14 +1,17 @@
 set -e #exit if any command return non zero status
-set -x #debug mode
+# set -x #debug modeset
+set -u #error and exit when env var is not set
 
-# Build docker image
-echo "================== Building an docker image =================="
 
 # login to docker hub
 echo "================== logging into docker hub =================="
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 
+# Build docker image
+echo "================== Building an docker image =================="
+docker build -t sachinmundhe/call-forwarding:latest .
 
 # push image to docker hub
 echo "================== pushung image to docker hub =================="
-
+docker push sachinmundhe/call-forwarding:latest
 
