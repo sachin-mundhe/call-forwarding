a74c4759756f0b14e081d0a3c66f3767d287a286
sachin-mundhe <sachinmundhe77@gmail.com>
2025-04-02 10:13:21 +0530

    fix: test demo

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/call-forwarding/config"
 	"github.com/call-forwarding/database"
 	"github.com/call-forwarding/server"
 )
 
 func main() {
+	log.Println("Call forwarding demo")
 	conf := config.GetConfig()
 	db := database.NewHarperDatabase(conf)
 	server.NewEchoServer(conf, db).Start()
